70f74fa7316ce58853689d4964f534a95a1ab038
Eider <egalarraga@uni.eus>
2018-03-21 10:49:57 +0100

    --lasterketa klasea kendu

--- bertsioak2.py
+++ bertsioak2.py
@@ -10,18 +10,4 @@ if __name__ == '__main__':
 print('bertsioak2')
 print('bertsioak2')
 
-class Lasterketa:
-    def __init__(self,data,izena,herria,hasiera_tokia,helmuga_tokia,distantzia,desnibel_positiboa,desnibel_negatiboa):
-        self.data = data
-        self.izena = izena
-        self.herria = herria
-        self.hasiera_tokia = hasiera_tokia
-        self.helmuga_tokia = helmuga_tokia
-        self.distantzia = distantzia
-        self.desnibel_positiboa = desnibel_positiboa
-        self.desnibel_negatiboa = desnibel_negatiboa
-    def Bistaratu(self):
-        print(self.izena)
 
-lasterketa1 = Lasterketa('21/02/2018', 'Kalamendi', 'Ondarroa', 'Itsasaurre', 'Kalamendi punta', 10.6, 1200, 600)
-lasterketa1.Bistaratu()
